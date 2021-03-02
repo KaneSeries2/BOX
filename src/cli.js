38a225c730c750fa076f1b75adeb29980e550d09
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-02 12:19:45 -0500

    Base Structure CLI

---
+++ src/cli.js
@@ -0,0 +1,46 @@
+import arg from 'arg';
+
+function parseArgumentsIntoOptions(rawArgs){
+    const args = arg({
+        '--git': Boolean,
+        '--yes': Boolean,
+        '--install': Boolean,
+        '-g': '--git',
+        '-y': '--yes',
+        '-i': '--install'
+    },
+    {
+        argv: rawArgs.slice(2)
+    })
+    return{
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        returnInstall: args['--install'] || false,
+    };
+}
+
+async function promptForMissingOptions(options){
+    const defaultTemplate = 'JavaScript';
+    if(options.skipPrompts){
+        return{
+            ... options,
+            template: options.template || defaultTemplate
+        }
+    }
+    const questions = [];
+    if(!options.template){
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose which project template to use',
+            choices: ['JavaScript', 'TypeScript'],
+            defualt: defaultTemplate
+        })
+    }
+}
+
+export function cli(args){
+    let options = parseArgumentsIntoOptions(args);
+    console.log(options);
+}

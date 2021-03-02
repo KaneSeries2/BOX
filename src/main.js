9ac1e6a560e6bbdec81b442f221253dca787edaa
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-02 13:10:14 -0500

    template directory w/ git initialize

--- src/main.js
+++ src/main.js
@@ -3,3 +3,26 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options){
+    return copy(options.templateDirectory, options.templateDirectory, {
+        clobber: false,
+    });
+}
+
+export async function createProject(options){
+    options = {
+        ... options,
+        targetDirectory: options.targetDirectory || process.cwd(),
+    };
+    const currentFileUrl = import.meta .url;
+    const templateDir = path.resolve(
+        new URL(currentFileUrl).pathname,
+        '../../templates',
+        options.template.toLowerCase()
+    );
+    options.templateDirectory = templateDir;
+}
